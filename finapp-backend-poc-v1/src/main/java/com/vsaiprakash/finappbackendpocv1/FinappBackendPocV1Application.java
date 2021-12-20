6fbd3d470b8eabf3485b43a8591aa0d71cd63e3a
V. Sai Prakash <vukkumsp@gmail.com>
2021-12-20 17:17:47 +0000

    app initialization done;

---
+++ finapp-backend-poc-v1/src/main/java/com/vsaiprakash/finappbackendpocv1/FinappBackendPocV1Application.java
@@ -0,0 +1,13 @@
+package com.vsaiprakash.finappbackendpocv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinappBackendPocV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinappBackendPocV1Application.class, args);
+	}
+
+}
---
+++ finapp-backend-poc-v1/src/test/java/com/vsaiprakash/finappbackendpocv1/FinappBackendPocV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsaiprakash.finappbackendpocv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinappBackendPocV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
